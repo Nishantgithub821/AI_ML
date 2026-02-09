34141eb17e39de5caf2f69a6fde0cf9e622bdb84
Nishantgithub821 <nb7180764@gmail.com>
2026-02-09 20:05:22 +0530

    Create Langchain_Part1_Intro.py

---
+++ Langchain_Part1_Intro.py
@@ -0,0 +1,31 @@
+                              USER
+                               ↓
++-------------------------------------------------------------+
+|                                                             |
+|  +----------------------------+   +----------------------+ |
+|  |  Building Basic LLM Apps   |   |        AGENTS        | |
+|  |                            |   |                      | |
+|  |  • Open Source vs Closed   |   |                      | |
+|  |    Source LLMs             |   |                      | |
+|  |                            |   |                      | |
+|  |  • Using LLM APIs          |   |                      | |
+|  |    - LangChain             |   |                      | |
+|  |    - HuggingFace           |   |                      | |
+|  |    - Ollama                |   |                      | |
+|  +----------------------------+   +----------------------+ |
+|                                                             |
+|  +-------------------+  +------------------+               |
+|  | Prompt Engineering|  |        RAG       |               |
+|  +-------------------+  +------------------+               |
+|                                                             |
+|  +-------------------+  +------------------+               |
+|  |   Fine Tuning     |  |      LLMOps      |               |
+|  +-------------------+  +------------------+               |
+|                                                             |
+|                    +---------------------+                  |
+|                    |    Miscellaneous    |                  |
+|                    +---------------------+                  |
+|                                                             |
++-------------------------------------------------------------+
+
+This is our structure of the studing the whole things right so first we start with the langchain then we go to langgraph and agentic ai right 
