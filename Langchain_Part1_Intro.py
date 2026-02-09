bd64522321f7fc2142622d38502e6372d720176e
Nishantgithub821 <nb7180764@gmail.com>
2026-02-09 20:11:34 +0530

    Update Langchain_Part1_Intro.py

--- Langchain_Part1_Intro.py
+++ Langchain_Part1_Intro.py
@@ -29,3 +29,57 @@
 +-------------------------------------------------------------+
 
 This is our structure of the studing the whole things right so first we start with the langchain then we go to langgraph and agentic ai right 
+
+#So the first thing is introduction of longchain 
+"""
+LangChain is an open-source framework that helps in building LLM-based applications. It provides modular components and end-to-end tools that help developers build complex AI applications such as chatbots, question-answering systems, retrieval-augmented generation (RAG), autonomous agents, and more.
+
+Key Points:
+
+It supports all major LLMs.
+
+It simplifies the development of LLM-based applications.
+
+It provides integration for all major tools.
+
+It is open-source, free, and actively developed.
+
+It supports a wide range of AI use cases.
+"""
+# Explanatino of each 5 points is
+"""
+1️⃣ Supports all major LLMs: LangChain sabhi tarah ke LLMs (OpenAI, Google, open-source, etc.) ke sath kaam karta hai bina app badle.
+
+2️⃣ Simplifies development: LangChain me pehle se backend code aur boilerplate hota hai isliye zero se code likhne ki zarurat nahi padti aur time-cost bachta hai.
+
+3️⃣ Tool integrations: LangChain databases, vector stores, APIs, tools aur external services ke sath easily integrate ho jata hai.
+
+4️⃣ Open-source & free: LangChain free, open-source hai aur regularly new features aur updates milte rehte hain.
+
+5️⃣ Wide AI use cases: LangChain chatbots, RAG, agents, Q&A systems, automation aur many AI applications support karta hai.
+"""
+"""
+So we cover the things like linewise so waht is the order of that is 
+1. Fundermantels 
+a) what is lanchain 
+b) odels 
+c) prompts 
+d) prompts 
+e) parsing output 
+f) runnables and LCEL
+g) Chains 
+H) Memory
+
+2) RAG
+a)Document Loaders 
+b) text splitters 
+c) embeddings 
+d) vector  stores 
+e) retrivers 
+f) buidliign a rag application 
+
+3) Agents 
+a) Tools and Toolkits 
+b) Tool calling 
+c) Building an ai agents 
+
