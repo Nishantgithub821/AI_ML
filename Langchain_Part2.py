bf10f87c55cb61fb5427ce0205098c369bdf54f6
Nishantgithub821 <nb7180764@gmail.com>
2026-02-09 21:50:29 +0530

    Create Langchain_Part2.py

---
+++ Langchain_Part2.py
@@ -0,0 +1,9 @@
+What is langchain 
+Why do we need langchain 
+High Level discussion of app 
+benefits of langchain 
+what can you build with langchain 
+
+SO Basically wht is langchain it is an open source framework for developling applications powered by llms 
+Now we udnerstand this thing more briefly->
+Now next thing is i explain the in depth working of the one project we talk about it and we discuss everything on it right 
