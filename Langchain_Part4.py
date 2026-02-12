34763637166e2787555a4fce1dcd70518b628a0d
Nishantgithub821 <nb7180764@gmail.com>
2026-02-12 09:37:11 +0530

    Expand explanation of language and embedding models
    Added explanation of language and embedding models, including practical implementation details and sources.

--- Langchain_Part4.py
+++ Langchain_Part4.py
@@ -1,6 +1,25 @@
 WHAT IS MODEL-> THIS IS IN DEPTH PART FOR THE ONLY MODEL COMPONENT IN LANCHAIN
+"""
 The model component in langchain is a cruicial part of the framework desgined to faciliate intereactions with variaous language models and embedding models 
 Iska matlab kya hai ki model component hame facility provide kerwata hai different type of models se itneract kerne me like there are two of them one is language modes and another one is embedding models right 
 As e also discussed wah is language model and embedding modelBut i also again expalin -> 
 language model like LLms and chatbot in which text to text is there if we give input it returns the output as text foramte like chatgpt 
 But in embeddding model it did the symentic search ok and we all know if you check out my previous documentation on it you know what is sementic search .
+symentic search -> Rag based system build help of embedding models . ( output as numbers not text) 
+
+"""
+# Now in this video we see the practial of it right we done so much theortical now we do practical of it so let start 
+# now hows the flowchhart of it right so 
+"""
+First we do language models in which thereaer two tye of source we do 
+Closed source 
+Open source 
+in closed source----> open Ai , claude gemini on these we work 
+in open source  -----> hugging face 
+WHEN THESE ARE COMPLETED THEN WE MOVE ON OUR EMBEDDING MODELS IN WHICH 
+closed source -> open ai embedding model 
+open source -> hugging face embedding model and then we create small chatbot okey 
+
+"""
+
+# Language Models :
